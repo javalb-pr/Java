d8359af462ee7f747a17c9b83f91c0d8e936c78d
Nikitoz9 <47643249+Nikitoz9@users.noreply.github.com>
2019-09-15 14:23:52 +0300

    Add files via upload

---
+++ src/by/gsu/pms/Race.java
@@ -0,0 +1,49 @@
+package by.gsu.pms;
+
+import java.io.Serializable;
+
+public class Race implements Serializable {
+    private String date;
+    private int numberOfRacers;
+    private boolean prize;
+
+    public Race(String date, int numberOfRacers, boolean prize) {
+        this.date = date;
+        this.numberOfRacers = numberOfRacers;
+        this.prize = prize;
+    }
+    public Race(String date, int numberOfRacers){
+        this.date = date;
+        this.numberOfRacers = numberOfRacers;
+        this.prize = true;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getNumberOfRacers() {
+        return numberOfRacers;
+    }
+
+    public void setNumberOfRacers(int numberOfRacers) {
+        this.numberOfRacers = numberOfRacers;
+    }
+
+    public boolean isPrize() {
+        return prize;
+    }
+
+    public void setPrize(boolean prize) {
+        this.prize = prize;
+    }
+
+    @Override
+    public String toString() {
+        return date+";"+numberOfRacers+";"+prize;
+    }
+}
